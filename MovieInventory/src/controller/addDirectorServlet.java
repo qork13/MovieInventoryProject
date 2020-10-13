869ab492dc1ce027fe253634c43218d66a49e938
qork13 <57408249+qork13@users.noreply.github.com>
2020-10-13 10:18:39 -0500

    Update addDirectorServlet.java
    Added

---
+++ MovieInventory/src/controller/addDirectorServlet.java
@@ -0,0 +1,47 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Director;
+
+/**
+ * Servlet implementation class addDirectorServlet
+ */
+@WebServlet("/addDirectorServlet")
+public class addDirectorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public addDirectorServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String name = request.getParameter("directorName");
+		Director director = new Director(name);
+		DirectorHelper dh = new DirectorHelper();
+		dh.insertDirector(director);
+		getServletContext().getRequestDispatcher("/viewAllDirectorsServlet").forward(request, response);
+	}
+
+}
